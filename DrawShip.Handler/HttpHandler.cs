5288eed28a6dff1b75b3ef837ce771484190fa30
Simon Laing <laingster@gmail.com>
2015-12-15 20:54:49 +0000

    Initial hello-world http handler

---
+++ DrawShip.Handler/HttpHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+
+namespace DrawShip.Handler
+{
+	public class HttpHandler : IHttpHandler
+	{
+		public bool IsReusable
+		{
+			[DebuggerStepThrough]
+			get { return true; }
+		}
+
+		public void ProcessRequest(HttpContext context)
+		{
+			var request = context.Request;
+			var response = context.Response;
+
+			response.StatusCode = 200;
+			response.Write("hello world" + request.Url);
+		}
+	}
+}
