07fea267fb370e3ee61e9c2081c6f02391d40d80
Simon Laing <laingster@gmail.com>
2015-12-22 21:05:55 +0000

    Introduced the concept of shapes to specify the templates that should be loaded for all previews

---
+++ DrawShip.Common/Shapes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+
+namespace DrawShip.Common
+{
+	public static class Shapes
+	{
+		static Shapes()
+		{
+			var shapeNames = ConfigurationManager.AppSettings["ShapeNames"];
+
+			if (!string.IsNullOrEmpty(shapeNames))
+				Names = shapeNames.Split(new[] { ';', ',' }, StringSplitOptions.RemoveEmptyEntries);
+		}
+
+		/// <summary>
+		/// The names of the shapes that should be included in the previews
+		/// </summary>
+		public static string[] Names { get; set; }
+
+		public static string GetNames()
+		{
+			if (Names == null)
+				return string.Empty;
+
+			return string.Join(";", Names);
+		}
+	}
+}
