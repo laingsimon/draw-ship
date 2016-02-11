1b345b908a17ec01265e87bb4b52ef2564ae1452
Simon Laing <laingster@gmail.com>
2016-02-11 18:28:08 +0000

    Introduced installer from a network share

---
+++ DrawShip.Viewer/install_from_share.bat
@@ -0,0 +1,33 @@
+@echo off
+set installDir=%ProgramFiles%\DrawShip
+
+if not exist %installDir% mkdir "%installDir%" 2> nul
+
+if not exist "%installDir%" (
+	set installDir=%SystemDrive%\DrawShip
+	mkdir "%installDir%" 2> nul
+)
+
+echo Installing to %installDir%
+
+set installDirEscaped=%installDir:\=\\%
+
+xcopy "*.*" "%installDir%\" /Y /h
+"%installDir%\DrawShip.Viewer.exe" /mode:install
+
+echo Windows Registry Editor Version 5.00 > "%installDir%\register_manually.reg"
+echo. >> "%installDir%\register_manually.reg"
+echo [HKEY_CLASSES_ROOT\xmlfile\shell\Preview in DrawShip] >> "%installDir%\register_manually.reg"
+echo. >> "%installDir%\register_manually.reg"
+echo [HKEY_CLASSES_ROOT\xmlfile\shell\Preview in DrawShip\command] >> "%installDir%\register_manually.reg"
+echo @="\"%installDirEscaped%\\DrawShip.Viewer.exe\" \"%%1\"" >> "%installDir%\register_manually.reg"
+echo. >> "%installDir%\register_manually.reg"
+echo @REM [HKEY_CLASSES_ROOT\xmlfile\shell\Preview in DrawShip (image)] >> "%installDir%\register_manually.reg"
+echo. >> "%installDir%\register_manually.reg"
+echo [HKEY_CLASSES_ROOT\xmlfile\shell\Preview in DrawShip (image)\command] >> "%installDir%\register_manually.reg"
+echo @="\"%installDirEscaped%\\DrawShip.Viewer.exe\" \"%%1\" /format:Image" >> "%installDir%\register_manually.reg"
+echo. >> "%installDir%\register_manually.reg"
+
+if "%errorlevel%"=="0" @echo Installed successfully
+
+pause
