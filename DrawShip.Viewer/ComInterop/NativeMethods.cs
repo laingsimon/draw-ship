9aeb8ec52e0abf7f4e2c9b704394e3da7097d611
Simon Laing <laingster@gmail.com>
2016-06-18 08:47:14 +0100

    Removed unused win-api methods

--- DrawShip.Viewer/ComInterop/NativeMethods.cs
+++ DrawShip.Viewer/ComInterop/NativeMethods.cs
@@ -35,15 +35,6 @@ namespace DrawShip.Viewer.ComInterop
 			int cch);
 
 		/// <summary>
-		/// Free the specified storage medium.
-		/// </summary>
-		/// <param name="pmedium">
-		/// Reference of the storage medium that is to be freed.
-		/// </param>
-		[DllImport("ole32.dll", CharSet = CharSet.Unicode)]
-		public static extern void ReleaseStgMedium(ref STGMEDIUM pmedium);
-
-		/// <summary>
 		/// Insert a new menu item at the specified position in a menu.
 		/// </summary>
 		/// <param name="hMenu">
@@ -73,22 +64,6 @@ namespace DrawShip.Viewer.ComInterop
 			[MarshalAs(UnmanagedType.Bool)]bool fByPosition,
 			ref MENUITEMINFO mii);
 
-		/// <summary>
-		/// The DeleteObject function deletes a logical pen, brush, font, bitmap,
-		/// region, or palette, freeing all system resources associated with the
-		/// object. After the object is deleted, the specified handle is no longer
-		/// valid.
-		/// </summary>
-		/// <param name="hObject">
-		/// A handle to a logical pen, brush, font, bitmap, region, or palette.
-		/// </param>
-		/// <returns>
-		/// If the function succeeds, the return value is true.
-		/// </returns>
-		[DllImport("gdi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-		public static extern bool DeleteObject(IntPtr hObject);
-
-
 		public static int HighWord(int number)
 		{
 			return ((number & 0x80000000) == 0x80000000) ?
@@ -105,16 +80,6 @@ namespace DrawShip.Viewer.ComInterop
 
 		[DllImport("user32")]
 		internal static extern IntPtr CreatePopupMenu();
-
-		[DllImport("user32")]
-		internal static extern bool InsertMenuItem(IntPtr hmenu, uint uposition, uint uflags, ref MENUITEMINFO mii);
-
-		// MF_BYCOMMAND MF_BYPOSITION MF_STRING MF_POPUP
-		[DllImport("user32")]
-		internal static extern bool AppendMenu(IntPtr hmenu, MFMENU uflags, IntPtr uIDNewItemOrSubmenu, string text);
-
-		[DllImport("user32")]
-		internal static extern bool InsertMenu(IntPtr hmenu, int position, MFMENU uflags, IntPtr uIDNewItemOrSubmenu, string text);
 
 		[StructLayout(LayoutKind.Sequential)]
 		public struct COPYDATASTRUCT
@@ -163,6 +128,5 @@ namespace DrawShip.Viewer.ComInterop
 			Marshal.FreeHGlobal(ptr);
 			ptr = IntPtr.Zero;
 		}
-
 	}
 }
