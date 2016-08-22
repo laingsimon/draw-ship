2471b644a42889edf9684006bc3191ab945d4ac2
Simon Laing <Simon.Laing@fundslibrary.co.uk>
2016-08-22 09:08:12 +0100

    Prevent duplicate key exception; support some more shape names

--- DrawShip.Common/DiagramExtensions.cs
+++ DrawShip.Common/DiagramExtensions.cs
@@ -45,8 +45,8 @@ namespace DrawShip.Common
 
 			var styleDictionary = (from pair in style.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
 								   let match = Regex.Match(pair, @"^(?<key>.+?)=(?<value>.+?)$")
-								  where match.Success
-								  select new { key = match.Groups["key"].Value, value = match.Groups["value"].Value })
+								   where match.Success && match.Groups["key"].Value == "shape"
+								   select new { key = match.Groups["key"].Value, value = match.Groups["value"].Value })
 								  .ToDictionary(a => a.key, a => a.value);
 
 			return styleDictionary.ContainsKey("shape")
@@ -56,9 +56,9 @@ namespace DrawShip.Common
 
 		private static string _GetShapeName(string fullShapeName)
 		{
-			var match = Regex.Match(fullShapeName, @"^mxgraph\.(?<shape>.+?)\.(?<category>.+)$");
+			var match = Regex.Match(fullShapeName, @"^mxgraph\.(?<shape>.+?)\.(?<category>[^.]+)$");
 			return match.Success
-				? match.Groups["shape"].Value
+				? match.Groups["shape"].Value.Replace(".", "/")
 				: null;
 		}
 	}
